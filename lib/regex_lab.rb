d47a3526b7cf73515283a5c8322a0705ea3e08d0
Unsar Malik <unsar.malik24@gmail.com>
2019-04-27 02:30:54 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+if word.match(/\A[aeiou]/i)==nil
+  return false
+  else
+  return true
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/\aun\w+ing\a/)
 end
 
 def words_five_letters_long(text)
